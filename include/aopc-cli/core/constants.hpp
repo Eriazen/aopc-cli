397e70c598a3af4edec0364a5fc6665d0c465cdf
vecerniv <vita.vecernik@seznam.cz>
2026-04-08 22:41:50 +0200

    update qualities in constants, trailing backslash in price api

--- include/aopc-cli/core/constants.hpp
+++ include/aopc-cli/core/constants.hpp
@@ -12,12 +12,13 @@ namespace constants {
     inline constexpr std::string_view API_EUROPE_URL { "https://europe.albion-online-data.com" };
     inline constexpr std::string_view API_AMERICAS_URL { "https://west.albion-online-data.com" };
     inline constexpr std::string_view API_ASIA_URL { "https://east.albion-online-data.com" };
-    inline constexpr std::string_view API_PRICE_ENDPOINT { "/api/v2/stats/prices" };
+    inline constexpr std::string_view API_PRICE_ENDPOINT { "/api/v2/stats/prices/" };
     inline constexpr std::string_view API_GOLD_ENDPOINT { "/api/v2/stats/gold.json?" };
 
     // Albion Online values
-    inline constexpr std::array<std::string_view, 7> CITIES { "Caerleon", "Lymhurst", "Bridgewater", "Martlock", "Thetford", "Fort Sterling", "Brecilien" };
-    inline constexpr std::array<std::string_view, 5> ITEM_QUALITIES { "Normal", "Good", "Outstanding", "Excellent", "Masterpiece" };
+    inline constexpr std::array<std::string_view, 7> CITIES { "Caerleon", "Lymhurst", "Bridgewater", "Martlock", "Thetford", "FortSterling", "Brecilien" };
+    //inline constexpr std::array<std::string_view, 5> ITEM_QUALITIES { "Normal", "Good", "Outstanding", "Excellent", "Masterpiece" };
+    inline constexpr std::array<std::string_view, 5> ITEM_QUALITIES { "1", "2", "3", "4", "5" };
 }
 
 #endif // CONSTANTS_HPP
