af175509f70496eada352930f2519187864c692d
vecerniv <vita.vecernik@seznam.cz>
2026-04-11 21:32:23 +0200

    move invalid command to header

--- include/aopc-cli/commands/invalidCommand.hpp
+++ include/aopc-cli/commands/invalidCommand.hpp
@@ -5,5 +5,8 @@
 
 class InvalidCommand : public Command {
     public:
-        void execute(const std::vector<std::string>& args) override;
+        void execute(const std::vector<std::string>& args) override {
+            (void)args; // Avoid unused parameter warning
+            std::cout << "Invalid command. Type 'help' for a list of available commands." << std::endl;
+        }
 };
--- src/commands/invalidCommand.cpp
+++
@@ -1,8 +0,0 @@
-#include "aopc-cli/commands/invalidCommand.hpp"
-#include<iostream>
-
-
-void InvalidCommand::execute(const std::vector<std::string>& args) {
-    (void)args; // Avoid unused parameter warning
-    std::cout << "Invalid command. Type 'help' for a list of available commands." << std::endl;
-}
