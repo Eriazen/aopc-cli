9d0405c9a116cc345b2402a9e159cceca83040a0
vecerniv <vita.vecernik@seznam.cz>
2026-04-13 13:20:24 +0200

    add ansi color support on windows

--- include/aopc-cli/io/formatter.hpp
+++ include/aopc-cli/io/formatter.hpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 
 namespace TextFormatter {
     std::string formatNumber(int value);
@@ -32,4 +36,17 @@ namespace TextFormatter {
             currentLineLength += static_cast<int>(word.length());
         }
     }
+}
+
+inline void enableVirtualTerminal() {
+#ifdef _WIN32
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) return;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) return;
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+#endif
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "aopc-cli/core/commandHandler.hpp"
 #include "aopc-cli/core/settings.hpp"
+#include "aopc-cli/io/formatter.hpp"
 
 
 CommandHandler handler;
 Settings& settings = Settings::getInstance();
 
 int main(int, char** argv){
+    // Enable ANSI color codes on windows
+    enableVirtualTerminal();
+
     // Initialize settings and command handler
     std::filesystem::path exeDir = std::filesystem::absolute(argv[0]).parent_path();
     std::filesystem::path dbPath = exeDir / "database" / "aopc_items.db";
